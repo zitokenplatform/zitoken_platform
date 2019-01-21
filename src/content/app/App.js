64d77ac74af5dc66fa90093ca7c3e7a88bc65b38
Jorge William Rodrigues de Almeida <jorge.almeida@fogas.local>
2019-01-21 12:55:30 -0400

    add links

--- src/content/app/App.js
+++ src/content/app/App.js
@@ -11,29 +11,29 @@ class App extends Component {
         <div className={style.AppHeader}>
           <h1 className={style.AppLogo}>Zitoken</h1>
           <p className={style.AppP}>Grow your coins playing on our game platform & Earn interest rates holding your BTC with us</p>
-          <button variant="contained" className={style.AppButtom}>
+          <button  className={style.AppButtom}>
             START PLAYING
           </button>
         </div>
         <div className={style.content}>
           <h1>OVERVIEW</h1>
           <strong>About Zitoken Platform</strong>
-          <p>Zitoken platform is a blockchain game platform, on which is possible to play different games
+          <p className={style.textContent}>Zitoken platform is a blockchain game platform, on which is possible to play different games
             such as trading games and strategy games using ZITOKEN ZIT (our utility token). There is
             another funtion that allows users to earn interest rates holding their BTC with us in a level
             of security in (cold wallet and hot wallet). <br /><br />
-            <a href="#">Whitepaper</a>
+            <a href="/whitepaper/Whitepaper-Zitoken.pdf">Whitepaper</a>
           </p>
           <strong>Zitoken (ZIT)</strong>
-          <p>Zitoken (ZIT) is our utility token and fuel on our platform, community can use it to fund their accounts
+          <p className={style.textContent}>Zitoken (ZIT) is our utility token and fuel on our platform, community can use it to fund their accounts
             to play all games on the platform and to participate at our official events. Zitoken (ZIT) is already available
             to buy on couple of exchanges and soon we plan to add our utility token on more exchanges.
           </p>
           <strong>Token Specifications</strong>
-          <p>Protocol: ERC20<br /><br />
-            <a href="#">Explorer</a>
+          <p className={style.textContent}>Protocol: ERC20<br /><br />
+            <a href="https://etherscan.io/token/0x9c04f0dc68c61efd2fc289987393a78b4a2494ea">Explorer</a>
           </p>
-          <p>
+          <p className={style.textContent}>
             Name: Zitoken <br />
             Symbol: ZIT <br />
             Decimals: 18 <br />
@@ -55,8 +55,8 @@ class App extends Component {
         <div className={style.AppExchange}>
           <h1>Exchanges</h1>
           <Grid container className={style.AppRodmap}>
-            <Grid item><a href="#"><img src="/img/forkDex.png" className={style.imgExchnange} /></a></Grid>
-            <Grid item><a href="#"><img src="/img/tokenDex.png" className={style.imgExchnange} /></a></Grid>
+            <Grid item><a href="https://forkdelta.app/#!/trade/0x9c04f0dc68c61efd2fc289987393a78b4a2494ea-ETH" target="_blank"><img src="/img/forkDex.png" className={style.imgExchnange} /></a></Grid>
+            <Grid item><a href="https://tokendex.github.io/#!/trade/0x9c04f0dc68c61efd2fc289987393a78b4a2494ea-ETH" target="_blank"><img src="/img/tokenDex.png" className={style.imgExchnange} /></a></Grid>
           </Grid>
         </div>
         <div className={style.contenFuture}>
@@ -74,11 +74,11 @@ class App extends Component {
           </div>
           <div className={style.AppFootLink}>
             <Grid container spacing={16}>
-              <Grid item><a href="#">Github</a></Grid>
+              <Grid item><a href="https://github.com/ZitokenProject">Github</a></Grid>
               <Grid item><a href="#">Bitcointalk</a></Grid>
-              <Grid item><a href="#">Twitter</a></Grid>
-              <Grid item><a href="#">Discord</a></Grid>
-              <Grid item><a href="#">Support</a></Grid>
+              <Grid item><a href="https://www.twitter.com/zitoken">Twitter</a></Grid>
+              <Grid item><a href="https://discord.gg/zPWqBQ">Discord</a></Grid>
+              <Grid item><a href="mailto:support@zioken.me">Support</a></Grid>
             </Grid>
           </div>
         </div>
