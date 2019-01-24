623f5f2efb9bf7938d3068311090b1d0314b0cc8
Jorge William Rodrigues de Almeida <jorge.almeida@fogas.local>
2019-01-24 12:22:21 -0400

    add text update

--- src/content/app/App.js
+++ src/content/app/App.js
@@ -4,6 +4,9 @@ import Grid from '@material-ui/core/Grid';
 import style from "./App.css";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
   onStart = () => {
     alert("Under development, check it out our Roadmap");
   }
@@ -14,7 +17,7 @@ class App extends Component {
         <StatusBar />
         <div className={style.AppHeader}>
           <h1 className={style.AppLogo}>Zitoken</h1>
-          <p className={style.AppP}>Grow your coins playing on our game platform & Earn interest rates holding your BTC with us</p>
+          <p className={style.AppP}>Use ZITOKEN (ZIT) as credit to play all games on our platform</p>
           <button  className={style.AppButtom} onClick={()=>this.onStart()}>
             START PLAYING
           </button>
@@ -22,10 +25,8 @@ class App extends Component {
         <div className={style.content}>
           <h1>OVERVIEW</h1>
           <strong>About Zitoken Platform</strong>
-          <p className={style.textContent}>Zitoken platform is a blockchain game platform, on which is possible to play different games
-            such as trading games and strategy games using ZITOKEN ZIT (our utility token). There is
-            another funtion that allows users to earn interest rates holding their BTC with us in a level
-            of security in (cold wallet and hot wallet). <br /><br />
+          <p className={style.textContent}>Zitoken platform is a blockchain game platform, on which is possible to play different games such as trading games and strategy games using ZITOKEN ZIT (our utility token) , platform is 
+            been designed to support a wide range of games developed on Blockchain Tecnology, initially the platform will support only games developed on Ethereum Blockchain, but gradually will support others protocols. <br /><br />
             <a href="/whitepaper/Whitepaper-Zitoken.pdf">Whitepaper</a>
           </p>
           <strong>Zitoken (ZIT)</strong>
@@ -50,10 +51,22 @@ class App extends Component {
           <Grid container className={style.AppRodmap} spacing={16}>
             <Grid item xs={12} sm={2}><p className={style.textBlue}>Q4 2018</p><p className={style.textSmall}>Core Development</p></Grid>
             <Grid item xs={12} sm={2}><p className={style.textBlue}>Q1 2019</p><p className={style.textSmall}>Beta Launch</p></Grid>
-            <Grid item xs={12} sm={2}><p className={style.textBlue}>Q4 2019</p><p className={style.textSmall}>First implementation of new games + Update</p></Grid>
-            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q1 2020</p><p className={style.textSmall}>Second implementation of new games + Update</p></Grid>
-            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q4 2020</p><p className={style.textSmall}>Third implementation  of new games + Update</p></Grid>
+            <Grid item xs={12} sm={2}><p className={style.textBlue}>Q2 2019</p><p className={style.textSmall}>First implementation of new games + Update</p></Grid>
+            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q2 2020</p><p className={style.textSmall}>Second implementation of new games + Update</p></Grid>
+            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q1 2021</p><p className={style.textSmall}>Third implementation  of new games + Update</p></Grid>
           </Grid>
+          
+          <h1>Team</h1>          
+          <strong>MANAGEMENT</strong>
+          <p>Leandro Alves, CEO Zitoken</p>
+          <p>Leandro is founder of PW7 , a blockchain engagement agency that provides Exchange Listing, Token Creation (ERC20) and Bounty Programs on TGE,  he also contributed on different other blockchain projects as consultant/growth hacker such as SID (Share Internet Data) and Investa.
+             <br />https://www.linkedin.com/in/leandro-alves-122665137             
+          </p>
+          <strong>PROJECT TEAM / LEADER DEVELOPER</strong>
+          <p>Jorge Almeida, Leader Developer Zitoken </p>
+          <p>Software engineer/System analyst.
+             <br />https://www.linkedin.com/in/jorge-william-90586b54/            
+          </p>
         </div>
 
         <div className={style.AppExchange}>
@@ -65,24 +78,24 @@ class App extends Component {
         </div>
         <div className={style.contenFuture}>
           <h1>FEATURES</h1><br />
-          <strong>Earn coins</strong>
           <ul>
-            <li>Playing on our game platform</li>
-            <li>Claiming free coins on our platform</li>
-            <li>Holding your BTC with us to receive interest rates</li>
-          </ul>
-        </div>
+            <li>Use ZITOKEN (ZIT) as credit to play all games on our platform</li>
+            <li>Claim free ZIT on our platform</li>
+            <li>Earn bonus to play specific games</li>
+          </ul>          
+        </div>        
         <div className={style.AppFoot}>
           <div className={style.textFoot}>
             <spam>© 2019 Zitoken.me - All rights reserved</spam>
           </div>
+
           <div className={style.AppFootLink}>
             <Grid container spacing={16}>
               <Grid item><a href="https://github.com/ZitokenProject">Github</a></Grid>
-              <Grid item><a href="#">Bitcointalk</a></Grid>
+              <Grid item><a href="https://bitcointalk.org/index.php?topic=5101230">Bitcointalk</a></Grid>
               <Grid item><a href="https://www.twitter.com/zitoken">Twitter</a></Grid>
               <Grid item><a href="https://discord.gg/zPWqBQ">Discord</a></Grid>
-              <Grid item><a href="mailto:support@zioken.me">Support</a></Grid>
+              <Grid item><a href="mailto:support@zitoken.me">Support</a></Grid>
             </Grid>
           </div>
         </div>
