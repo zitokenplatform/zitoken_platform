3e6059ab55a4502ed54b7ca8ad4f5ac9a6b9a6ac
Djalma Bina <36211080+donovan780@users.noreply.github.com>
2019-02-11 01:07:54 -0200

    Add files via upload

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -12,13 +12,11 @@ class HomePage extends React.Component {
     handleDeleteUser(id) {
         return (e) => this.props.dispatch(userActions.delete(id));
     }
-
     render() {
         const { user, users } = this.props;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <p><img src="tokenDex.png" /> </p>
-                <h1>Hi {user.firstName}!</h1>
+                <h1>Hi {user.username}!</h1>
                 <p>You're logged in with React!!</p>
                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
@@ -27,7 +25,7 @@ class HomePage extends React.Component {
                     <ul>
                         {users.items.map((user, index) =>
                             <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
+                                {user.username + ' ' + user.username}
                                 {
                                     user.deleting ? <em> - Deleting...</em>
                                     : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
