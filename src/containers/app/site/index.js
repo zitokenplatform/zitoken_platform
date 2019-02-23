e5edb551593db6c9c8437453357eec0d71d2f098
Jorge William <jorgewra@gmail.com>
2019-02-22 23:26:16 -0400

    update deployment + add develop

--- src/containers/app/site/index.js
+++ src/containers/app/site/index.js
@@ -4,31 +4,34 @@ import { connect } from "react-redux";
 import StatusBar from '../../../components/statusBar';
 
 import site from '../../site/index';
+import register from '../../register/index'
+import errorNotFound from '../../errosPages/errorNotFound';
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
-    
+
     render() {
         return (
             <Router>
                 <div>
-                <StatusBar />
-                <Switch>
-                    {/* INSIDE ROUTES */}
-                    <Route path="/" exact={true} component={site} />
-                  
-                    {/* ERRORS PAGE */}
-                    <Route path="/404" component={site} />
-                    <Route path="/500" component={site} />
-                    <Route path={"**"} component={site} />
-                </Switch>
+                    <StatusBar />
+                    <Switch>
+                        {/* INSIDE ROUTES */}
+                        <Route path="/" exact={true} component={site} />
+                        <Route path="/register" exact={true} component={register} />
+
+                        {/* ERRORS PAGE */}
+                        <Route path="/404" component={errorNotFound} />
+                        <Route path="/500" component={errorNotFound} />
+                        <Route path={"**"} component={errorNotFound} />
+                    </Switch>
                 </div>
             </Router>
-            );
+        );
     }
 }
 
-  
+
 const mapSateToProps = store => ({
 });
 export default connect(mapSateToProps)(App);
