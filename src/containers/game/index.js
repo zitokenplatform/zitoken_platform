09e5a49ed7e770d407bbdfe5a1bb9885aa5a971a
Djalma Bina <36211080+donovan780@users.noreply.github.com>
2019-03-07 00:47:24 -0300

    Add files via upload

--- src/containers/game/index.js
+++ src/containers/game/index.js
@@ -1,13 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class Game extends Component {
-    render() {
+const styles = {
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+};
+
+ function MediaCard(props) {
+        const { classes } = props;
         return (
-            <div>
-                <h1>Teste</h1>
-            </div>
-        );
+            <Card className={classes.card}>
+    <CardActionArea>
+        <CardMedia
+        className={classes.media}
+        image="game-1.jpg"
+        title="GAMES"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            Games 1
+            </Typography>
+            <Typography component="p">
+            Here is  coming  new  game !
+        </Typography>
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        <Button size="small" color="primary">
+            Share
+            </Button>
+            <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+        </Card>
+    );
     }
-}
 
-export default Game;
+MediaCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+
+
+export default withStyles(styles)(MediaCard);
