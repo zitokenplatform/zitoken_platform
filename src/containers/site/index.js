eb52ba30ec6ecd0d449e7d8d43383486fa816a6e
Djalma Bina <36211080+donovan780@users.noreply.github.com>
2019-05-02 09:58:56 -0300

    Add files via upload

--- src/containers/site/index.js
+++ src/containers/site/index.js
@@ -29,31 +29,31 @@ class Site extends Component {
     return (
       <div className={style.App}>
         <div className={style.AppHeader}>
-          <h1 className={style.AppLogo}>Zitoken</h1>
-          <p className={style.AppP}>Use ZITOKEN (ZIT) as credit to play all games on our platform</p>
+          <h1 className={style.AppLogo}>Blockchain Game Crowdfunding</h1>
+          <p className={style.AppP}>Use ZITOKEN (ZIT) as credit to support  the development of  amazing games on our platform</p>
           <button className={style.AppButtom} onClick={() => this.onStart()}>
-            START PLAYING
+            CREATE  CAMPAIGN
           </button>
         </div>
         <div className={style.content} id="overview" >
           <h1>OVERVIEW</h1>
           <strong>About Zitoken Platform</strong>
           <p className={style.textContent}>
-          Zitoken platform is a blockchain game platform, on which is possible to play different games such as strategy games with (characters and gods) using ZITOKEN ZIT (our utility token) , platform is been designed to support a wide range of games developed on Blockchain Tecnology, initially the platform will support only games developed on Ethereum Blockchain, but gradually will support others protocols.
-          <br /><br />
+            Zitoken platform is a blockchain game crowdfunding, on which is possible to fund the development of different games such as strategy games with (characters and gods) using ZITOKEN ZIT as credit. Platform is been designed to support a wide range of games developed on Blockchain Technology.
+            Initially the platform will accept only ZIT as payment method and soon we will be considering new payment methods to contribute.
+            <br /><br />
             <a href="/whitepaper/Whitepaper-Zitoken.pdf">Whitepaper</a>
           </p>
           <strong>Zitoken (ZIT)</strong>
-          <p className={style.textContent}>Zitoken (ZIT) is our utility token and fuel on our platform, community can use it to fund their accounts
-            to play all games on the platform and to participate at our official events. Zitoken (ZIT) is already available
-            to buy on couple of exchanges and soon we plan to add our utility token on more exchanges.
+          <p className={style.textContent}>ZITOKEN (ZIT)  is our utility token and fuel on our platform, community/users can use it to fund the development of games and to participate
+            at our official events. ZITOKEN (ZIT) is already available to buy on couple of exchanges and soon we plan to add  our utility token on more exchanges.
           </p>
           <strong>Token Specifications</strong>
           <p className={style.textContent}>Protocol: ERC20<br /><br />
             <a href="https://etherscan.io/token/0x9c04f0dc68c61efd2fc289987393a78b4a2494ea">Explorer</a>
           </p>
           <p className={style.textContent}>
-            Name: Zitoken <br />
+            Name:  ZITOKEN   <br />
             Symbol: ZIT <br />
             Decimals: 18 <br />
             Total Supply: 988 million <br /><br />
@@ -65,23 +65,24 @@ class Site extends Component {
           <Grid container className={style.AppRodmap} spacing={16}>
             <Grid item xs={12} sm={2}><p className={style.textBlue}>Q4 2018</p><p className={style.textSmall}>Core Development</p></Grid>
             <Grid item xs={12} sm={2}><p className={style.textBlue}>Q2 2019</p><p className={style.textSmall}>Beta Launch</p></Grid>
-            <Grid item xs={12} sm={2}><p className={style.textBlue}>Q4 2019</p><p className={style.textSmall}>First implementation of new games + Update</p></Grid>
-            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q2 2020</p><p className={style.textSmall}>Second implementation of new games + Update</p></Grid>
-            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q1 2021</p><p className={style.textSmall}>Third implementation  of new games + Update</p></Grid>
+            <Grid item xs={12} sm={2}><p className={style.textBlue}>Q4 2019</p><p className={style.textSmall}>First Update to accept new game protocols </p></Grid>
+            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q2 2020</p><p className={style.textSmall}>Second Update to accept new game protocols </p></Grid>
+            <Grid item xs={12} sm={3}><p className={style.textBlue}>Q1 2021</p><p className={style.textSmall}>Third Update to accept new game protocols </p></Grid>
           </Grid>
 
           <h1>Team</h1>
           <strong>MANAGEMENT</strong>
           <p>Leandro Alves, General Manager Zitoken</p>
-          <p>Leandro is founder of PW7 , a blockchain engagement agency that provides Exchange Listing, Token Creation (ERC20) and Bounty Programs on TGE,  he also contributed on different other blockchain projects as consultant/growth hacker such as SID (Share Internet Data) and Investa.
-             <br />https://www.linkedin.com/in/leandro-alves-122665137
+          <p>Leandro is founder of PW7 , a blockchain engagement agency that provides Exchange Listing,
+            he also contributed on different other blockchain projects as consultant/growth hacker such as SID (Share Internet Data).
+            <br />https://www.linkedin.com/in/leandro-alves-122665137
           </p>
-          <strong>PROJECT TEAM / LEADER DEVELOPER</strong>
+          <strong>CONTRIBUTOR / PARTNER DEVELOPER</strong>
           <p>Jorge Almeida, Leader Developer Zitoken </p>
           <p>Software engineer/System analyst.
              <br />https://www.linkedin.com/in/jorge-william-90586b54/
           </p>
-          <strong>DEVELOPER</strong>
+          <strong>CONTRIBUTOR / PARTNER DEVELOPER </strong>
           <p>Djalma Bina, Developer Zitoken</p>
           <p>Front-end/UX.
              <br />https://www.linkedin.com/in/djalmabina/
@@ -103,11 +104,10 @@ class Site extends Component {
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-              Zitoken platform is a blockchain game platform, on which is possible to play
-              different games such as strategy games with (characters and gods) using ZITOKEN
-              ZIT (our utility token). Platform is been designed to support a wide range of games
-              developed on Blockchain Tecnology.
-          </Typography>
+                Zitoken platform is a blockchain game crowdfunding, on which is possible to fund the development of different games such as strategy games with
+                (characters and gods) using ZITOKEN ZIT (our utility token) as credit.
+                Platform is been designed to support a wide range of games developed on Blockchain Technology.
+              </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <ExpansionPanel>
@@ -142,31 +142,25 @@ class Site extends Component {
           </ExpansionPanel>
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>How does blockchain game developers make money on our platform? </Typography>
+              <Typography className={classes.heading}>
+                  Will game developers receive any amount raised on our platform even if not reach the goal ?
+              </Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-              We pay 40% of the total revenue generated with every game. This payment is credited in the last day of the month to game developers.
+                  Yes. Developers will receive their funds even if not reach the goal.
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Zitoken (ZIT) is it required to play all games on our platform? </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-              Yes. ZIT is required as credit to play all games on Zitoken platform.
+              <Typography className={classes.heading}>
+                  Zitoken platform takes any fee of the total raised?
               </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className={classes.heading}>Is the price fixed to buy tokens/credits?</Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
               <Typography>
-              Price may vary on different exchanges and on our platform, for this reason we are making partnerships only with trusted exchanges to avoid suspect schemes
+                  We take 10% of the total raised on our platform.
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -174,9 +168,15 @@ class Site extends Component {
         <div className={style.contenFuture} id="features">
           <h1>FEATURES</h1><br />
           <ul>
-            <li>Use ZITOKEN (ZIT) as credit to play all games on our platform</li>
-            <li>Claim free ZIT on our platform</li>
-            <li>Earn bonus to play specific games</li>
+            <li>
+                Use ZITOKEN (ZIT) as credit to support all games on our platform
+            </li>
+            <li>
+                Earn rewards to support the development of specific games.
+            </li>
+            <li>
+                Earn free ZIT on our bounties and airdrops.
+            </li>
           </ul>
         </div>
         <div className={style.AppFoot}>
